f89d21fefb5575197da78ea1e374faad144f460c
guta231 <gustavoho2005@gmail.com>
2025-09-20 17:46:20 -0300

    alterando icone do site

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" type="image/webp" href="{{ asset('favicon.webp') }}">
+
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
